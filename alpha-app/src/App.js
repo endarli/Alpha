1a1ff63a215926924ee985241a4e38dfdee9d4b6
Endar <endar.li215@gmail.com>
2022-10-16 17:18:49 -0400

    add table and form

--- alpha-app/src/App.js
+++ alpha-app/src/App.js
@@ -1,6 +1,7 @@
-// import logo from './logo.svg';
 import './App.css';
-import { Component} from "react";
+import { Component } from "react";
+import { Table, TableContainer, TableHead, TableBody, TableRow, TableCell, Paper} from '@mui/material';
+import { AppBar, Toolbar, TextField, Button, Box } from "@material-ui/core";
 const request = require("request");
 const util = require("util");
 // https://docs.npmjs.com/cli/v8/commands/npm-link for folders outside
@@ -10,7 +11,8 @@ class App extends Component {
     super(props);
 
     this.state = {
-      data: [],
+      sumbitted: false,
+      fLet: null,
       women: [],
       search: {}
     };
@@ -18,11 +20,11 @@ class App extends Component {
     //Binding event listeners
     this.searchWoman = this.searchWoman.bind(this);
     this.allWomen = this.allWomen.bind(this);
+    this.handleSubmit = this.handleSubmit(this);
   }
 
   async componentDidMount(){
     try {
-      this.searchWoman();
       this.allWomen();
 
     } catch(e) {
@@ -31,33 +33,14 @@ class App extends Component {
     }
   }
 
-  //Search Woman Handler
-  async searchWoman(event) {
-    try {
-      let requestPromise = util.promisify(request);
-      let response = await requestPromise({
-        method: 'GET',
-        headers: {
-          "Content-Type": "application/json",
-        },
-        uri: "http://localhost:4000/woman/E"
-      });
-      if (response) {
-        let data = JSON.parse(response.body);
-        this.setState({
-          search: {
-            wID: data[0].woman_id,
-            fLetter: data[0].fLetter,
-            wName: data[0].wName,
-            wParrafo: data[0].wParrafo,
-            wBday: data[0].wBday,
-          }
-        });
-      }
-    } catch(e) {
-      console.log("search");
-      console.log(e);
-    }
+  handleSubmit(e) {
+    e.preventDefault();
+    this.setState({
+      sumbitted: true,
+      fLet: document.getElementById('fL').value
+    })
+
+    this.searchWoman();
   }
 
   // All Woman Handler
@@ -84,18 +67,80 @@ class App extends Component {
     }
   }
 
+  //Search Woman Handler
+  async searchWoman(event) {
+    try {
+      let requestPromise = util.promisify(request);
+      let response = await requestPromise({
+        method: 'GET',
+        headers: {
+          "Content-Type": "application/json",
+        },
+        uri: "http://localhost:4000/woman/"+JSON.stringify(fLet)
+      });
+      if (response) {
+        let data = JSON.parse(response.body);
+        this.setState({
+          search: {
+            wID: data[0].woman_id,
+            fLetter: data[0].fLetter,
+            wName: data[0].wName,
+            wParrafo: data[0].wParrafo,
+            wBday: data[0].wBday,
+          }
+        });
+      }
+    } catch(e) {
+      console.log("search");
+      console.log(e);
+    }
+  }
+
   render() {
+    var search = this.state.search;
     return (
-       <div>
-        <p>Hello</p>
-            {
-                this.state.data.map((search)=>
-                <div>
-                    <span>{search.wName} </span>
-                    <span>{search.wParrafo}</span>
-                </div>
-                )
-            }
+       <div className="App">
+        <h className="App-header">
+          <p>Alpha</p>
+        </h>
+
+        <form onSubmit={handleSubmit}>
+          First Letter of Your Name:
+          <TextField
+            style={{ width: "200px", margin: "5px" }}
+            type="text"
+            label="please format in Uppercase"
+            variant="outlined"
+            id="fL"
+          />
+          <br />
+          <br />
+          <Button type="submit" variant="contained" color="primary">
+            Submit
+          </Button>
+        </form>
+
+        <TableContainer component={Paper}>
+          <Table>
+            <TableHead>
+              <TableRow>
+                <TableCell>ID</TableCell>
+                <TableCell align="left">First Letter of Name</TableCell>
+                <TableCell align="left">Name</TableCell>
+                <TableCell align="left">About</TableCell>
+                <TableCell align="left">Birthday</TableCell>
+              </TableRow>
+            </TableHead>
+
+            <TableBody>
+              {Object.keys(search).map(function(value, idx) {
+                return <TableCell key={idx}>
+                  {search[value]}
+                </TableCell>
+              })}
+            </TableBody>
+          </Table>
+        </TableContainer>
         </div>
     );
   }
